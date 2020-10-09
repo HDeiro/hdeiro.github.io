3328a0a6290d74a897a8067d852d5bba4fed7e88
Hugo Deiró <hugodeiro@gmail.com>
2020-10-09 03:19:48 -0300

    Section Skills

--- app/app.js
+++ app/app.js
@@ -1,4 +1,29 @@
-window.__forceSmoothScrollPolyfill__ = true;
+window.onload = () => {
+	const language = navigator.language === 'pt-BR' ? 'pt-br' : 'en-us';
+
+	fetch(`i18n/${language}.json`)
+		.then(response => response.json())
+		.then(data => Object.keys(data).forEach(key => {
+			const element = document.querySelector(`#${key}`);
+
+			if (element) {
+				const value = data[key];
+				const itsJustAString = value[0].length === 1;
+
+				if (itsJustAString) {
+					element.innerHTML = value;
+				} else {
+					value.forEach(valueItem => {
+						const node 	   = document.createElement('div');
+						node.innerHTML = valueItem;
+						element.appendChild(node);
+					});
+				}
+			}
+		}))
+
+}
+
 
 function navigateTo(target) {
 	if (target) {
